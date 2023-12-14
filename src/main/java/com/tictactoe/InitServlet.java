2a952b8c7df4a343a24d6b2153cbf45ba6c6d7bb
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2023-12-14 14:15:48 +0100

    user placement of ticks and automatic placement of zeroes addedÐ

--- src/main/java/com/tictactoe/InitServlet.java
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet(name="InitServlet", value="/start")
+@WebServlet(name = "InitServlet", value = "/start")
 public class InitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
